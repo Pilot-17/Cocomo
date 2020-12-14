dde23c6452078e0edbfd6be2aa5a6b7cf6083d3c
Almat <almat.kulbaev@yandex.ru>
2020-12-15 01:05:32 +0700

    Updates

---
+++ precache-manifest.f224fd852ef73e9542320236cbecaae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9955e0ab3725e0e183f9516a30117e18",
+    "url": "/Cocomo/index.html"
+  },
+  {
+    "revision": "6dd80bc00dfedbef03c0",
+    "url": "/Cocomo/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "6b8b2b8d6b6f5ecc4978",
+    "url": "/Cocomo/static/css/main.d7f82884.chunk.css"
+  },
+  {
+    "revision": "6dd80bc00dfedbef03c0",
+    "url": "/Cocomo/static/js/2.6a8d44dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Cocomo/static/js/2.6a8d44dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8b2b8d6b6f5ecc4978",
+    "url": "/Cocomo/static/js/main.1b83be54.chunk.js"
+  },
+  {
+    "revision": "7200166597b4656d398f",
+    "url": "/Cocomo/static/js/runtime-main.ad8c344a.js"
+  }
+]);
