6da595435278a03ee362662c0c648662a2cf3243
Almat Kulbaev <almat.kulbaev@yandex.ru>
2020-12-18 00:18:09 +0700

    Updates

--- precache-manifest.b89ef94e41907fe14259613c93009415.js
+++ precache-manifest.b89ef94e41907fe14259613c93009415.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7590a36118511cd7a69d482d4bff6dae",
+    "revision": "86c15fa2b6f1aaa5aa2bc09bcf6b25ab",
     "url": "/Cocomo/index.html"
   },
   {
-    "revision": "600d2f658cb6d78a1986",
+    "revision": "a627ecb6160292dc99af",
     "url": "/Cocomo/static/css/2.a2026422.chunk.css"
   },
   {
-    "revision": "33feb0e7ae05b73c3458",
+    "revision": "19074cbab0072eed8151",
     "url": "/Cocomo/static/css/main.d7f82884.chunk.css"
   },
   {
-    "revision": "600d2f658cb6d78a1986",
-    "url": "/Cocomo/static/js/2.10631199.chunk.js"
+    "revision": "a627ecb6160292dc99af",
+    "url": "/Cocomo/static/js/2.fade831e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Cocomo/static/js/2.10631199.chunk.js.LICENSE.txt"
+    "url": "/Cocomo/static/js/2.fade831e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "33feb0e7ae05b73c3458",
-    "url": "/Cocomo/static/js/main.039ca3bb.chunk.js"
+    "revision": "19074cbab0072eed8151",
+    "url": "/Cocomo/static/js/main.ee243d39.chunk.js"
   },
   {
     "revision": "7200166597b4656d398f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Cocomo/precache-manifest.beba63731b4c45cf8fbc7d7ba0d002f4.js"
+  "/Cocomo/precache-manifest.b89ef94e41907fe14259613c93009415.js"
 );
 
 self.addEventListener('message', (event) => {
